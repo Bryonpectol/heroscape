ebda8cf92a07f087cab88abb11bea996cc9ba928
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-04-20 22:16:11 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 let mainImg = document.getElementById("mainImg");
-
+let card = document.getElementById("changecard");
+let cardValue = card.value;
 
 function changeCard() {
-   mainImg.setAttribute("src", "/images/jandar_straight_blank_960603_original.png");
+   if(cardValue == "jandar")
+   {
+      mainImg.setAttribute("src", "/images/jandar_straight_blank_960603_original.png");
+   }
 }
