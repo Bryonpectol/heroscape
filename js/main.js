0658c92febfcdc3be544cca45e3b79c785f4e89f
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-04-20 22:41:18 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,22 +8,22 @@ function onChange() {
 
   if(value == "jandar")
   {
-    mainImg.setAttribute("src", "images/jandar_straight_blank_960603_original.png");
+    mainImg.setAttribute("src", "/images/jandar_straight_blank_960603_original.png");
   }
 
   if(value == "ullar")
   {
-    mainImg.setAttribute("src", "images/ullar_straight_blank_693955_original.png");
+    mainImg.setAttribute("src", "/images/ullar_straight_blank_693955_original.png");
   }
 
   if(value == "utgar")
   {
-    mainImg.setAttribute("src", "images/utgar_straight_blank_original.png");
+    mainImg.setAttribute("src", "/images/utgar_straight_blank_original.png");
   }
 
   if(value == "vydar")
   {
-   mainImg.setAttribute("src", "");
+   mainImg.setAttribute("src", "/images/vydar_straight_blank_963207_original.png");
   }
 }
 e.onchange = onChange;
