15b73501c1c706ff56c670072f925659ddb3f4de
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2023-04-20 21:58:42 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ let mainImg = document.getElementById("mainImg");
 
 
 function changeCard() {
-   mainImg.setAttribute("src", "");
+   mainImg.setAttribute("src", "/images/jandar_straight_blank_960603_original.png");
 }
